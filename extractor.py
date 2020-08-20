8fdfb5293ef8ee5dedda2eae5b500d3ea19ee4d4
Benjamin Saljooghi <benjamin.saljooghi@gmail.com>
2020-08-21 07:38:09 +1000

    Update extractor.py

--- extractor.py
+++ extractor.py
@@ -1,8 +1,19 @@
 from pathlib import Path
 import subprocess
 
-directory = "2"
+directory = "gradebook_format"
 
-for item in Path(directory).iterdir():
-    args = ["unar", str(item.name), "-o", item.stem]
+def switch(file: Path):
+    if file.suffix == ".zip":
+        return ["7z", "x", file.name, f"-o{file.stem}"]
+    elif file.suffix == ".rar":
+        return ["unrar", "x", file.name, f"{file.stem}/"]
+    else:
+        print(f"unknown {file}")
+        raise Exception
+
+
+for item in Path(directory).iterdir():    
+    args = switch(item)
+    print(args)
     subprocess.Popen(args, cwd=directory)
